cb5c42f446ad5e64f977a267bf6c37060094499c
Артюх Сергей <sergei.artyukh@informicus.ru>
2023-10-08 14:42:49 +0300

    #40 избавился от DataStorageService

--- TinkoffInvestStatistic/Infrastructure/Infrastructure/Services/IPositionService.cs
+++ TinkoffInvestStatistic/Infrastructure/Infrastructure/Services/IPositionService.cs
@@ -14,27 +14,27 @@ namespace Infrastructure.Services
         /// <summary>
         /// Возвращает список позиций определенного типа по счету.
         /// </summary>
-        /// <param name="accountId">Идентификатор счета.</param>
+        /// <param name="accountNumber">Идентификатор счета.</param>
         /// <param name="positionType">Тип позиции.</param>
         /// <returns>Группированный список позиций только для чтения.</returns>
-        public Task<IReadOnlyCollection<Position>> GetPositionsByTypeAsync(string accountId, PositionType positionType);
+        public Task<IReadOnlyCollection<Position>> GetPositionsByTypeAsync(string accountNumber, PositionType positionType);
 
         /// <summary>
         /// Сохранение данных позиций по счету.
         /// </summary>
-        /// <param name="accountId">Номер счета.</param>
+        /// <param name="accountNumber">Номер счета.</param>
         /// <param name="positionType">Тип позиций.</param>
         /// <param name="data">Данные по позициям</param>
-        public Task SavePlanPercents(string accountId, PositionType positionType, PositionData[] data);
+        public Task SavePlanPercents(string accountNumber, PositionType positionType, PositionData[] data);
 
         /// <summary>
         /// Добавляет планируемую для покупки позицию.
         /// </summary>
-        /// <param name="accountId">Номер счета.</param>
+        /// <param name="accountNumber">Номер счета.</param>
         /// <param name="type">Тип позиции.</param>
         /// <param name="figi">Финансовый идентификатор.</param>
         /// <param name="name">Наименование.</param>
         /// <param name="ticker">тикер.</param>
-        public Task AddPlannedPositionAsync(string accountId, PositionType type, string figi, string name, string ticker);
+        public Task AddPlannedPositionAsync(string accountNumber, PositionType type, string figi, string name, string ticker);
     }
 }
--- TinkoffInvestStatistic/Services/CurrencyService.cs
+++ TinkoffInvestStatistic/Services/CurrencyService.cs
@@ -1,6 +1,11 @@
 ﻿using Domain;
 using Infrastructure.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 using System.Threading.Tasks;
+using TinkoffInvestStatistic.Contracts;
+using Xamarin.Forms;
 
 namespace Services
 {
@@ -8,9 +13,46 @@ namespace Services
     public class CurrencyService : ICurrencyService
     {
         /// <inheritdoc/>
-        public Task SavePlanPercents(string accountNumber, CurrencyData[] data)
+        public Task SavePlanPercents(string accountNumber, CurrencyData[] currenciesData)
         {
-            return DataStorageService.Instance.SaveCurrenciesData(accountNumber, data);
+            if (string.IsNullOrEmpty(accountNumber))
+            {
+                throw new ArgumentNullException(nameof(accountNumber), "Полученный номер счета не может быть неопределенным.");
+            }
+
+            if (currenciesData == null || currenciesData.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(currenciesData), "Полученные данные о валютах не могут быть неопределенными.");
+            }
+
+            var dataAccessService = DependencyService.Resolve<IDataStorageAccessService>();
+            return dataAccessService.SaveCurrenciesDataAsync(accountNumber, currenciesData);
+        }
+
+        /// <inheritdoc/>
+        public async Task<AccountCurrencyData[]> MergeCurrenciesDataAsync(string accountNumber, AccountCurrencyData[] currencies)
+        {
+            if (string.IsNullOrEmpty(accountNumber))
+            {
+                throw new ArgumentNullException(nameof(accountNumber), "Полученный номер счета не может быть неопределенным.");
+            }
+
+            if (currencies == null || !currencies.Any())
+            {
+                return Array.Empty<AccountCurrencyData>();
+            }
+
+            var dataAccessService = DependencyService.Resolve<IDataStorageAccessService>();
+            var currenciesEntities = await dataAccessService.GetCurrenciesDataAsync(accountNumber);
+
+            var result = new List<AccountCurrencyData>();
+            foreach (var currency in currencies)
+            {
+                var currencyEntity = currenciesEntities.FirstOrDefault(i => i.Currency == currency.Currency);
+                result.Add(new AccountCurrencyData(currency.Currency, currencyEntity?.PlanPercent ?? 0, currency.Sum));
+            }
+
+            return result.ToArray();
         }
     }
 }
